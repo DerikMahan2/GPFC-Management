464ecc27527bf875a6e3604d237dea2e13847966
DerikMahan2 <demahan1@buffs.wtamu.edu>
2024-05-09 13:34:16 -0500

    Updated

--- Pages/Matches/Details.cshtml.cs
+++ Pages/Matches/Details.cshtml.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -18,7 +15,7 @@ namespace GPFC_Management.Pages.Matches
             _context = context;
         }
 
-        public Match Match { get; set; } = default!;
+        public Match Match { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -27,15 +24,16 @@ namespace GPFC_Management.Pages.Matches
                 return NotFound();
             }
 
-            var match = await _context.Matches.FirstOrDefaultAsync(m => m.MatchId == id);
-            if (match == null)
+            Match = await _context.Matches
+                .Include(m => m.HomeTeam)
+                .Include(m => m.AwayTeam)
+                .FirstOrDefaultAsync(m => m.MatchId == id);
+
+            if (Match == null)
             {
                 return NotFound();
             }
-            else
-            {
-                Match = match;
-            }
+
             return Page();
         }
     }
